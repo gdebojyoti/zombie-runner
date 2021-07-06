06c06b41a5497b15eb546f6c748900ef13ff2c05
Debojyoti Ghosh <debojyoti.ghosh@housing.com>
2021-07-06 12:57:50 +0530

    Bullets have max range.

--- Assets/Scripts/Controllers/BulletController.cs
+++ Assets/Scripts/Controllers/BulletController.cs
@@ -2,20 +2,31 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour {
 
+  #region public members
+
+    public float speed = 200f;
+    public float maxRange = 8f; // max distance the bullet can travel before getting destroyed
+
+  #endregion
+
   #region private members
 
     private Rigidbody2D m_rb;
-    // private float m_maxDistance = 5f;
-    public float speed = 200f;
+    private Vector2 origin;
 
   #endregion
   
   private void Start () {
     Debug.Log("Fired!");
     m_rb = GetComponent<Rigidbody2D>();
+    origin = transform.position;
   }
 
-  private void FixedUpdate() {
+  private void Update () {
+    _CheckIfRangeExceeded();
+  }
+
+  private void FixedUpdate () {
     _Travel();
   }
 
@@ -39,6 +50,13 @@ public class BulletController : MonoBehaviour {
 
       // move self
       m_rb.velocity = new Vector2(1, 0) * speedPerFrame;
+    }
+
+    // if bullet has travelled beyond range, destroy it
+    private void _CheckIfRangeExceeded () {
+      if (((Vector2)transform.position - origin).magnitude >= maxRange) {
+        Destroy(gameObject);
+      }
     }
 
   #endregion
