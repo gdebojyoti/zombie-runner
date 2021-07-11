292694c6abad180cc8dfa29b968991afea12bd09
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2021-07-11 22:58:58 +0530

    Ground updated. (#5)
    * New ground.
    
    * Cleanup.
    
    * Enemy initialization.
    
    * Zombie prefab updated.
    
    * Player prefab updated.
    
    * Enemy positions altered.
    
    * Remove old "ground".
    
    Co-authored-by: Debojyoti Ghosh <debojyoti.ghosh@housing.com>

---
+++ Assets/Scripts/Controllers/GroundController.cs
@@ -0,0 +1,113 @@
+using UnityEngine;
+
+public class GroundController : MonoBehaviour {
+  
+  #region public members
+
+    public float baseMovementSpeed = -2f;
+    public GameObject groundPrefab;
+    public GameObject zombiePrefab;
+    public float width = 10f; // width of ground block (i.e., self)
+
+  #endregion
+
+  #region private members
+
+    private bool m_NextBlockInitialized = false;
+    private float m_cameraWidth; // width of camera, used to decide when to create & delete ground blocks
+    private string m_enemyName = "enemy";
+    private Vector2[] m_enemyPositions = new Vector2[]{
+      new Vector2(1,0),
+      new Vector2(3,1),
+      new Vector2(6,0),
+      new Vector2(8,-1),
+      new Vector2(9,1)
+    };
+
+  #endregion
+
+  #region MonoBehaviour methods
+
+    private void Start () {
+      // calculate camera width (TODO: can be moved to GameService)
+      Camera m_camera = Camera.main;
+      m_cameraWidth = m_camera.aspect * 2f * m_camera.orthographicSize;
+
+      _ClearOldObstacles();
+      _InitObstacles();
+    }
+
+    private void FixedUpdate () {
+      _Move();
+      _IsGroundNeeded();
+      _DidLeaveViewport();
+    }
+
+  #endregion
+
+  #region private methods
+
+    private void _Move() {
+      // speed to be applied in current frame
+      float speedPerFrame = baseMovementSpeed * GameService.GetWorldMovementMultiplier() * Time.fixedDeltaTime;
+
+      // move self
+      transform.Translate(speedPerFrame, 0, 0);
+    }
+
+    // check whether new ground block needs to be created
+    private void _IsGroundNeeded () {
+      if (m_NextBlockInitialized) {
+        return;
+      }
+      if ((transform.position.x + width) < m_cameraWidth) {
+        _CreateNextGround();
+      }
+    }
+
+    // check whether current ground block has left camera viewport
+    private void _DidLeaveViewport () {
+      if ((transform.position.x + width) < -m_cameraWidth) {
+        Destroy(gameObject);
+      }
+    }
+
+    // instantiate new ground block
+    private void _CreateNextGround () {
+      // instantiate new ground from self
+      GameObject newGround = Instantiate(
+        groundPrefab,
+        new Vector2(transform.position.x + width - .06f, transform.position.y),
+        Quaternion.identity,
+        transform.parent
+      );
+
+      newGround.name = "Ground";
+
+      // update flag
+      m_NextBlockInitialized = true;
+    }
+
+    private void _ClearOldObstacles () {
+      foreach (Transform child in transform) {
+        if (child.name == m_enemyName) {
+          Destroy(child.gameObject);
+        }
+      }
+    }
+
+    private void _InitObstacles () {
+      // generate obstacles only if block is outside camera view
+      if (transform.position.x < m_cameraWidth / 2) {
+        return;
+      }
+
+      foreach(Vector2 position in m_enemyPositions) {
+        GameObject enemy = Instantiate(zombiePrefab, transform.position, Quaternion.identity, transform);
+        enemy.transform.localPosition = position;
+        enemy.name = m_enemyName;
+      }
+    }
+
+  #endregion
+}
